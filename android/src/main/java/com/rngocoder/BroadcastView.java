3a0d6dde2f44e053a08fbf8e30eca96cea070070
Xin <yan.x@wustl.edu>
2017-07-05 15:56:53 -0500

    Remove stopCamera()

--- android/src/main/java/com/rngocoder/BroadcastView.java
+++ android/src/main/java/com/rngocoder/BroadcastView.java
@@ -175,6 +175,7 @@ public class BroadcastView extends FrameLayout implements LifecycleEventListener
     public boolean isBroadcasting() {
         return broadcasting;
     }
+    
 
     public void setBroadcasting(boolean broadcasting) {
         if(goCoder == null){
@@ -215,7 +216,6 @@ public class BroadcastView extends FrameLayout implements LifecycleEventListener
                         broadcast.putString("broadcastName", getBroadcastName());
                         broadcast.putString("status", "stopped");
                         event.putMap("event", broadcast);
-                        stopCamera();
                         mEventEmitter.receiveEvent(getId(), Events.EVENT_BROADCAST_STOP.toString(), event);
                     }
                 }
